f6f70f91804ead74b7f0b47ffb769803ebe3df61
Adrian Kryzinski <adriankryzinski@gmail.com>
2021-04-14 19:48:31 +0200

    View tasks in done / not done sections.

--- another-to-do-app/src/components/TaskList.js
+++ another-to-do-app/src/components/TaskList.js
@@ -2,7 +2,20 @@ import React from "react";
 import Task from "./Task";
 
 const TaskList = (props) => {
-  const tasks = props.tasks.map((task) => (
+  const active = props.tasks.filter((task) => task.active === true);
+  const done = props.tasks.filter((task) => task.active === false);
+  console.log(active, done);
+
+  const activeTasks = active.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      delete={props.delete}
+      change={props.change}
+    />
+  ));
+
+  const doneTasks = done.map((task) => (
     <Task
       key={task.id}
       task={task}
@@ -15,12 +28,19 @@ const TaskList = (props) => {
     <>
       <div className="active">
         <h1> Task's List</h1>
-        {tasks}
+        {activeTasks.length > 0 ? activeTasks : <p>No more tasks ! : )</p>}
       </div>
 
       <hr />
       <div className="done">
-        <h3> Done Task's (0)</h3>
+        <h3>
+          {" "}
+          Done Task's <em>({done.length})</em>
+        </h3>
+        {done.length > 3 && (
+          <span style={{ fontSize: 12 }}>Only last 3 tasks display !</span>
+        )}
+        {doneTasks.slice(0, 3)}
       </div>
     </>
   );
